e502b7735a477ec636a98cea1f4acc2ba53ffd94
David-Neto <davidrmachado@gmail.com>
2022-04-07 16:44:38 -0300

    exercicio 2

---
+++ 01 - Fundamentos/Bloco 04 - Introdução à JavaScript e Lógica de Programação/02-JavaScript-Array-e-loop-For/exercicio_1.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let number;
+
+//Exercício 1 - Percorra o array imprimindo todos os valores nele contidos com a função console.log().
+
+for (number of numbers){
+    console.log(number);
+}
