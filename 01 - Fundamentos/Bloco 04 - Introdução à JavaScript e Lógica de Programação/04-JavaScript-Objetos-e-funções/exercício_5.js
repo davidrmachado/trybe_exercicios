a1d964291a2725ca238e0fbb30d18caefd0df31b
David-Neto <davidrmachado@gmail.com>
2022-04-11 21:18:10 -0300

    exercício 5

---
+++ 01 - Fundamentos/Bloco 04 - Introdução à JavaScript e Lógica de Programação/04-JavaScript-Objetos-e-funções/exercício_5.js
@@ -0,0 +1,22 @@
+//Exercício 4 - Defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves:
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+}
+
+let inf = {
+    personagem: 'Tio Patinhas',
+    origem: "Chrismas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+}
+
+
+
+
+    for(let key in info,inf) {
+        console.log(info[key] + " e " + inf[key]);
+}
