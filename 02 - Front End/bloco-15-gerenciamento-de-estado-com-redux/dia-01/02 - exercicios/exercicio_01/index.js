b023d3e21117390b58eaeb1d0f39ceca224d542b
David-Neto <davidrmachado@gmail.com>
2022-07-21 21:57:48 -0300

    exercício 1 com cor aleatória

--- 02 - Front End/bloco-15-gerenciamento-de-estado-com-redux/dia-01/02 - exercicios/exercicio_01/index.js
+++ 02 - Front End/bloco-15-gerenciamento-de-estado-com-redux/dia-01/02 - exercicios/exercicio_01/index.js
@@ -1,14 +1,24 @@
 const showColor = (index) => {
-//   const { index, colors } = store.getState();
   const colorName = document.getElementById("value");
   colorName.innerHTML = index;
   const containerColor = document.getElementById("container");
   containerColor.style.backgroundColor = index;
 };
 
+function criarCor() {
+    const oneChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    let cor = '#';
+    const aleatorio = () => Math.floor(Math.random() * oneChar.length);
+    for (let i = 0; i < 6; i += 1) {
+        cor += oneChar[aleatorio()];
+    }
+    return cor;
+}
+
 // ========== ACTIONTYPES ==========
 const NEXT = "NEXT";
 const PREVIOUS = "PREVIOUS";
+const RANDOM = "RANDOM";
 
 // =========== ACTIONS ==========
 const actionNext = () => {
@@ -17,6 +27,12 @@ const actionNext = () => {
 
 const actionPrevious = () => {
   store.dispatch({ type: PREVIOUS });
+};
+
+const actionRandom = () => {
+    const { colors } = store.getState();
+    console.log(colors);
+  store.dispatch({ type: RANDOM });
 };
 
 // ========== REDUCER ==========
@@ -30,11 +46,17 @@ const reducer = (state = INITIAL_STATE, action) => {
     case "NEXT":
       return { 
         ...state,
-        index: state.index === 5 ? state.index : state.index + 1 };
+        index: state.index === (state.colors.length - 1) ? state.index : state.index + 1 };
     case "PREVIOUS":
       return { 
         ...state,
         index: state.index === 0 ? state.index : state.index - 1 };
+    case "RANDOM":
+      return { 
+        ...state,
+        colors: [...state.colors, criarCor()],
+        index: state.colors.length,
+      }
     default:
       return state;
   }
@@ -49,7 +71,10 @@ store.subscribe(() => {
 });
 
 const bttnNext = document.getElementById("next");
-const bttnPrevious = document.getElementById("previous");
-
 bttnNext.addEventListener("click", actionNext);
+
+const bttnPrevious = document.getElementById("previous");
 bttnPrevious.addEventListener("click", actionPrevious);
+
+const bttnRandom = document.getElementById("random");
+bttnRandom.addEventListener("click", actionRandom);
